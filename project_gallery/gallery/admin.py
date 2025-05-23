4cad2ef5bee380183c60ad7fed43fa4b45d5543b
VladRaFeed <jack22toplolkek@gmail.com>
2025-05-23 15:33:39 +0300

    add task

--- project_gallery/gallery/admin.py
+++ project_gallery/gallery/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Image, Category
 
-# Register your models here.
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image',)
+    filter_horizontal = ('categories',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- project_gallery/gallery/models.py
+++ project_gallery/gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='gallery_images/')
+    categories = models.ManyToManyField(Category, blank=True)
+    created_date = models.DateField()
+    age_limit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
